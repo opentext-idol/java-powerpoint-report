707f67f8a85c4b95c6698288efa009c685d43375
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-02-27 11:41:25 +0000

    Add package-info.java Javadoc for the two packages.

---
+++ src/main/java/com/hp/autonomy/frontend/reports/powerpoint/dto/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2017 Hewlett Packard Enterprise Development, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+/**
+ * Provides data transfer objects (DTOs) to represent data from various types of visualizations.
+ */
+package com.hp.autonomy.frontend.reports.powerpoint.dto;
---
+++ src/main/java/com/hp/autonomy/frontend/reports/powerpoint/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2017 Hewlett Packard Enterprise Development, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+/**
+ * Provides an API to generate PowerPoint reports from visualization data.
+ * @see <a href="https://github.com/hpe-idol/java-powerpoint-report/">README.md</a> for examples and usage instructions.
+ */
+package com.hp.autonomy.frontend.reports.powerpoint;
